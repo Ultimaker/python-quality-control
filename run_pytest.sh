52a2dc69ab0120a46377e797fc4491236ab07955
Alex Borro <alexborro@gmail.com>
2024-01-24 13:19:12 +0100

    Execute Sudo to see the output

--- run_pytest.sh
+++ run_pytest.sh
@@ -9,6 +9,12 @@ rm -rf ./.mypy_cache
 rm -rf ./cov_report
 rm -rf ./pstats
 
+echo "#####################   TESTING SUDO COMMAND"
+
+sudo -- dbus-daemon --fork --session --print-address
+
+echo "#####################   DONE"
+
 pytest -c ./ci/cfg/pytest.ini --cov=. --cov-report=html --cov-report=xml --cov-config=./ci/cfg/coverage.ini "${@}" --profile-svg --pstats-dir=./pstats
 
 py3clean .
