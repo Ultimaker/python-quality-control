e71359e1a1e61b5262ef9b6fea6df4ad91bf64a2
Alex Borro <alexborro@gmail.com>
2024-01-24 13:33:25 +0100

    Add more debug info

--- run_pytest.sh
+++ run_pytest.sh
@@ -10,9 +10,20 @@ rm -rf ./cov_report
 rm -rf ./pstats
 
 echo "#####################   TESTING SUDO COMMAND"
-
+set -x
+cat /etc/passwd
+echo "#####################"
+cat /etc/group
+echo "#####################"
+whoami
+echo "#####################"
+groups
+echo "#####################"
+sudo -l
+echo "#####################"
+sudo ls /
+echo "#####################"
 sudo -- dbus-daemon --fork --session --print-address
-
 echo "#####################   DONE"
 
 pytest -c ./ci/cfg/pytest.ini --cov=. --cov-report=html --cov-report=xml --cov-config=./ci/cfg/coverage.ini "${@}" --profile-svg --pstats-dir=./pstats
