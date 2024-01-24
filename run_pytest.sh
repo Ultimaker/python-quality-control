c3d913a15a3a7f1e9f7800eccd54afd7fb6d3c65
Alex Borro <alexborro@gmail.com>
2024-01-24 14:03:33 +0100

    Remove some debuging messages

--- run_pytest.sh
+++ run_pytest.sh
@@ -10,18 +10,7 @@ rm -rf ./cov_report
 rm -rf ./pstats
 
 echo "#####################   TESTING SUDO COMMAND"
-set -x
-cat /etc/passwd
-echo "#####################"
-cat /etc/group
-echo "#####################"
 whoami
-echo "#####################"
-groups
-echo "#####################"
-sudo -l
-echo "#####################"
-sudo ls /
 echo "#####################"
 sudo -- dbus-daemon --fork --session --print-address
 echo "#####################   DONE"
